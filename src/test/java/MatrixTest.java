297e1f79b2d3d2b7bd522e948dd40719e097d7a9
OleksiyDovgan <oleksiy.dovgan@gmail.com>
2023-09-18 15:57:55 +0200

    test 2

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -15,6 +15,21 @@ class MatrixTest {
     }
 
     @Test
+    void testIsSquareMatrixWithNull() {
+        int[][] testMatrix = null;
+        boolean result = new Matrix().isSquareMatrix(testMatrix);
+        assertFalse(result);
+    }
+
+    @Test
+    void testIsSquareMatrixWithEmptyMatrix() {
+        int[][] testMatrix = new int[0][0];
+        boolean result = new Matrix().isSquareMatrix(testMatrix);
+        assertFalse(result);
+    }
+
+
+    @Test
     void isSquareMatrix() {
         int[][] testMatrix = {
                 {1, 2, 3},
@@ -27,6 +42,32 @@ class MatrixTest {
     }
 
     @Test
+    void testIsSquareMatrixWithJaggedArray() {
+        int[][] testMatrix = {
+                {1, 2, 3},
+                {4, 5},
+                {7, 8, 9}
+        };
+        boolean result = new Matrix().isSquareMatrix(testMatrix);
+        assertFalse(result);
+    }
+
+    @Test
+    void testIsSquareMatrixWithNonSquareMatrix() {
+        int[][] testMatrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        boolean result = new Matrix().isSquareMatrix(testMatrix);
+        assertFalse(result);
+    }
+
+    @Test
     void averageArrayElements() {
+
+        int[] testArray = {1, 2, 3, 4, 5};
+        double result = new Matrix().averageArrayElements(testArray);
+        assertEquals(3.0, result, 0.001);
+
     }
 }
